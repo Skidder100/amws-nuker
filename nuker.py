8fee57ddc2debe53283dd6c600a4e1ca4e0b316e
Skidder100 <144185130+Skidder100@users.noreply.github.com>
2023-09-29 14:36:51 +1000

    Update nuker.py

--- nuker.py
+++ nuker.py
@@ -3,28 +3,26 @@ import time
 
 # ASCII art logo
 logo = """
-                                        /$$                                  /$$
-                                      | $/                                 | $$
-  /$$$$$$  /$$$$$$/$$$$  /$$  /$$  /$$|_//$$$$$$$       /$$$$$$$  /$$   /$$| $$   /$$  /$$$$$$   /$$$$$$
- |____  $$| $$_  $$_  $$| $$ | $$ | $$  /$$_____/      | $$__  $$| $$  | $$| $$  /$$/ /$$__  $$ /$$__  $$
-  /$$$$$$$| $$ \ $$ \ $$| $$ | $$ | $$ |  $$$$$$       | $$  \ $$| $$  | $$| $$$$$$/ | $$$$$$$$| $$  \__/
- /$$__  $$| $$ | $$ | $$| $$ | $$ | $$  \____  $$      | $$  | $$| $$  | $$| $$_  $$ | $$_____/| $$
-|  $$$$$$$| $$ | $$ | $$|  $$$$$/$$$$/  /$$$$$$$/      | $$  | $$|  $$$$$$/| $$ \  $$|  $$$$$$$| $$
- \_______/|__/ |__/ |__/ \_____/\___/  |_______/       |__/  |__/ \______/ |__/  \__/ \_______/|__/
-
-
+                                        /$$                                  /$$                                
+                                      | $/                                 | $$                                
+  /$$$$$$  /$$$$$$/$$$$  /$$  /$$  /$$|_//$$$$$$$       /$$$$$$$  /$$   /$$| $$   /$$  /$$$$$$   /$$$$$$       
+ |____  $$| $$_  $$_  $$| $$ | $$ | $$  /$$_____/      | $$__  $$| $$  | $$| $$  /$$/ /$$__  $$ /$$__  $$      
+  /$$$$$$$| $$ \ $$ \ $$| $$ | $$ | $$ |  $$$$$$       | $$  \ $$| $$  | $$| $$$$$$/ | $$$$$$$$| $$  \__/      
+ /$$__  $$| $$ | $$ | $$| $$ | $$ | $$  \____  $$      | $$  | $$| $$  | $$| $$_  $$ | $$_____/| $$            
+|  $$$$$$$| $$ | $$ | $$|  $$$$$/$$$$/  /$$$$$$$/      | $$  | $$|  $$$$$$/| $$ \  $$|  $$$$$$$| $$            
+ \_______/|__/ |__/ |__/ \_____/\___/  |_______/       |__/  |__/ \______/ |__/  \__/ \_______/|__/            
 """
 
-def send_discord_message(webhook_url, message, channel, delay_seconds):
+def send_discord_message(webhook_url, message, channel, bot_name, delay_seconds):
     payload = {
         "content": message,
-        "username": "CustomBot"
+        "username": bot_name
     }
 
     try:
         response = requests.post(webhook_url, json=payload)
         response.raise_for_status()
-        print(f"Message sent to #{channel}: {message}")
+        print(f"Message sent to #{channel} by {bot_name}: {message}")
     except requests.exceptions.RequestException as e:
         print(f"Error sending message: {e}")
 
@@ -34,11 +32,12 @@ def main():
     webhook_url = input("Enter your Discord webhook URL: ")
     message = input("Enter the message you want to send: ")
     channel = input("Enter the channel name (without the '#'): ")
+    bot_name = input("Enter the bot name: ")
     delay_seconds = float(input("Enter the delay between messages (in seconds): "))
 
     while True:
-        send_discord_message(webhook_url, message, channel, delay_seconds)
-
+        send_discord_message(webhook_url, message, channel, bot_name, delay_seconds)
+        
 if __name__ == "__main__":
     print(logo)  # Display the ASCII art logo
     main()
